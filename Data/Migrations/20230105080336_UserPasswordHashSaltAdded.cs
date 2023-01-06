462fabe1c5787c489a46e04f810fe5cd6e4988e4
MaoFan <60415663+ferhanelfren@users.noreply.github.com>
2023-01-06 10:44:52 +0800

    End of Section 4

---
+++ Data/Migrations/20230105080336_UserPasswordHashSaltAdded.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserPasswordHashSaltAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "BLOB",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PaswordSalt",
+                table: "Users",
+                type: "BLOB",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PaswordSalt",
+                table: "Users");
+        }
+    }
+}
